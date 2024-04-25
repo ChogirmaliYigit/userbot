26cc99445e55b7a080744b670b69112e6fbc108f
Jahongir Ibragimov <118691897+ChogirmaliYigit@users.noreply.github.com>
2024-04-25 13:00:00 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from environs import Env
 env = Env()
 env.read_env()
 
-app = Client("userbot")
+app = Client("userbot", api_hash=env.str("API_HASH"), api_id=env.int("API_ID"))
 
 WHITE_LIST = env.list("WHITE_LIST")
 
