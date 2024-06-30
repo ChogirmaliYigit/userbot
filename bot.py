de406052a3e5e1a51d84bbfbb16f1a3531a2d78d
Jahongir Ibragimov <118691897+ChogirmaliYigit@users.noreply.github.com>
2024-06-30 19:48:17 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,32 +6,28 @@ env.read_env()
 
 app = Client("userbot", api_hash=env.str("API_HASH"), api_id=env.int("API_ID"))
 
-WHITE_LIST = env.list("WHITE_LIST")
-
-CHAT_IDS = env.list("CHAT_IDS")
+CHAT_ID = env.int("CHAT_ID")
 
 
 @app.on_message(filters.private)
 async def hello(client: Client, message: types.Message):
-    print(message)
     if not message.from_user.is_bot and message.from_user.id not in WHITE_LIST:
-        msg = "Iltimos, menga yozish uchun avval quyidagi kanallarga/guruhlarga obuna bo'ling/qo'shiling:\n\n"
-        count = 0
-        for chat_id in CHAT_IDS:
-            chat_id = int(chat_id)
-            try:
-                chat_member = await client.get_chat_member(chat_id, message.from_user.id)
-            except (errors.UserNotParticipant, KeyError):
-                chat_member = None
-            if not chat_member or chat_member.status not in [
-                enums.ChatMemberStatus.MEMBER,
-                enums.ChatMemberStatus.OWNER,
-                enums.ChatMemberStatus.ADMINISTRATOR,
-            ]:
-                chat = await client.get_chat(chat_id)
-                count += 1
-                msg += f"[{count}. {chat.title}]({chat.invite_link})\n"
-        if count > 0:
+        msg = """"MAXSUS FIZ - MAT" o'quv markazi tashabbusi bilan amalga oshirilayotgan "KUN SAVOLI" loyihasiga 
+
+         X U SH    K E L I B S I Z !!!
+
+Javob yo'llash uchun avval quyidagi kanalga obuna bo'lishingiz zarur!
+
+        @maxsus_fiz_mat"""
+        try:
+            chat_member = await client.get_chat_member(CHAT_ID, message.from_user.id)
+        except (errors.UserNotParticipant, KeyError):
+            chat_member = None
+        if not chat_member or chat_member.status not in [
+            enums.ChatMemberStatus.MEMBER,
+            enums.ChatMemberStatus.OWNER,
+            enums.ChatMemberStatus.ADMINISTRATOR,
+        ]:
             await message.forward("me")
             await message.delete()
             if message.document:
@@ -41,11 +37,21 @@ async def hello(client: Client, message: types.Message):
                        " kerak bo'ladi.](https://t.me/M_Fiz_Mat)\n\n**Natija har kuni kech soat 20:00 da "
                        "t.me/maxsus_fiz_mat telegram kanalida jonli efirda aniqlanadi.**\n\n[Kanalga obuna bo'lishni "
                        "unutmang!](https://t.me/MAXSUS_FIZ_MAT)")
-            await message.reply(
-                msg,
-                parse_mode=enums.ParseMode.MARKDOWN,
-                disable_web_page_preview=True,
-            )
+        else:
+            msg = """"KUN SAVOLI" loyihasida har kuni yangi savollar joylanadi. Har kuni kech soat 20:00 da @maxsus_fiz_mat kanalida jonli efir bo'ladi. 
+
+**Albatta qatnashing!** 
+
+To'g'ri javob yo'llaganlar o'rtasida "RANDOM" dasturi yordamida OMADLI ISHTIROKCHI aniqlanadi va pul mukofoti taqdim etiladi.
+
+Omadli ishtirokchi SIZ bo'lishingiz mumkin!
+
+Bizni kuzatishda davom eting. Bundan ham yaxshi loyihalarimiz hali oldinda!"""
+        await message.reply(
+            msg,
+            parse_mode=enums.ParseMode.MARKDOWN,
+            disable_web_page_preview=True,
+        )
 
 
 app.run()
